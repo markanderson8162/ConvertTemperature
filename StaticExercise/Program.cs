e4b8b0387212f79139a775f4b7a5a3ff99979b36
Mark Anderson <markanderson8162@gmail.com>
2021-06-23 13:59:28 -0500

    created Static exercise

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StaticExercise
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Convert F to C");
+			var celsius = double.Parse(Console.ReadLine());
+			Console.WriteLine($"{TempConverter.FahrenheitToCelsius(celsius)}");
+
+			Console.WriteLine("");
+
+			Console.WriteLine("Convert C to F");
+			var fahrenheit = double.Parse(Console.ReadLine());
+			Console.WriteLine($"{TempConverter.CelsiusToFahrenheit(fahrenheit)}");
+		}
+	}
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+		public static double FahrenheitToCelsius(double temp)
+		{
+			double celsius = (temp - 32) * 0.555;
+			return celsius;
+		}
+
+		public static double CelsiusToFahrenheit(double temp)
+		{
+			double fahrenheit = (temp * 1.8) + 32;
+			return fahrenheit;
+		}
+
+
+	}
+}
